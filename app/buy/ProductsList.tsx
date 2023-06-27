609715b6214d4a3108c56282997ebcd4d4996eb7
qwerty-fish <qwertyfish>
2023-06-27 20:38:26 +0300

    feat(app): added components for store page

---
+++ app/buy/ProductsList.tsx
@@ -0,0 +1,84 @@
+import ProductCard from "../ProductCard";
+
+const ProductsList = () => {
+  return (
+    <section className="flex flex-col bg-gray-100 rounded-lg p-5 space-y-3 drop-shadow-lg">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-8">
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+        <ProductCard
+          productName="Product"
+          description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+          price={250}
+          slogan={"GG"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ProductsList;
---
+++ app/buy/Search.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <section className="flex flex-col bg-gray-100 rounded-lg p-5 space-y-3 drop-shadow-lg">
+      <div className="flex flex-col space-y-3">
+        <form className="flex items-center drop-shadow-lg">
+          <label htmlFor="simple-search" className="sr-only">Search</label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 "
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  "
+              placeholder="Search"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+          >
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              >
+              </path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </form>
+        
+        <select className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 drop-shadow-lg">
+          <option selected>Relevance</option>
+          <option value="TR">Trending</option>
+          <option value="LA">Latest Arrivals</option>
+          <option value="LH">Price: Low to High</option>
+          <option value="HL">Price: High to Low</option>
+        </select>
+      </div>
+
+      <div className="flex items-center justify-center py-4 md:py-8 flex-wrap drop-shadow-lg">
+        <button
+          type="button"
+          className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 "
+        >
+          All categories
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 "
+        >
+          Shoes
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 "
+        >
+          Bags
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 "
+        >
+          Electronics
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 "
+        >
+          Gaming
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Search;
